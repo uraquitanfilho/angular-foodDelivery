b6b735340daa561551c35f662672b21e0d4642e4
Uraquitan Filho <uraquitan@syx.com.br>
2018-02-24 05:54:23 -0300

    rest api server, observable, services, http access to call rest API

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -12,7 +12,10 @@ export class RestaurantComponent implements OnInit {
   constructor(private restaurantService: RestaurantService) { }
 //inicialization of the component
   ngOnInit() {
-    this.restaurants = this.restaurantService.restaurants();
+    //async method
+    //Using the subscible we will have the access to the return of the service method using the observable
+    this.restaurantService.restaurants()
+      .subscribe(restaurants => this.restaurants = restaurants);
   }
 
 }
--- src/app/restaurant/restaurant.service.ts
+++ src/app/restaurant/restaurant.service.ts
@@ -1,28 +1,26 @@
+/*We will use another service "Http service" inside a service, so.. 
+  VERY IMPORTANT: TO use service inside service you need to use the decorator @Injectable()
+*/
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+
 import {Store} from './store/store.model';
 
+import { URL_API } from '../app.api'; //Was imported the CONST URL_API
+//WE will to use OBSERVABLE. REMEMBER? To work with REST API need to use RXJS
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Injectable()
 export class RestaurantService {
-    rests: Store[] = [
-        {
-          id: "bread-bakery",
-          name: "Bread & Bakery",
-          category: "Bakery",
-          deliveryEstimate: "25m",
-          rating: 4.9,
-          imagePath: "assets/img/restaurants/breadbakery.png"
-        },
-        {
-          id: "burger-house",
-          name: "Burger House",
-          category: "Hamburgers",
-          deliveryEstimate: "100m",
-          rating: 3.5,
-          imagePath: "assets/img/restaurants/burgerhouse.png"
-        }
-      ];    
-    constructor() {
+     
+    constructor(private http: Http) {
 
     }
-    restaurants(): Store[] {
-      return this.rests;
+    restaurants(): Observable<Store[]> {
+      //.map convert the result in a list of JSON 
+      //Response will return many informations but we need only the JSON.
+      return this.http.get(`${URL_API}/restaurants`)
+        .map(response => response.json());
     }
 }
