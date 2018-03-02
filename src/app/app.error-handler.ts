e4f0be555a14a2f327705bfbe2fe0f3afd5f3abb
Uraquitan Filho <uraquitan@syx.com.br>
2018-03-02 19:43:53 -0300

    zone to app.error.handler.ts

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,4 +1,4 @@
-import {ErrorHandler, Injectable, Injector} from '@angular/core';
+import {ErrorHandler, Injectable, Injector, NgZone} from '@angular/core';
 import {HttpErrorResponse} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
@@ -9,14 +9,19 @@ import { LoginService } from './security/login/login.service';
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
     
-    constructor(private ns: NotificationService, private injector : Injector) {
+    constructor(private ns: NotificationService, 
+                private injector : Injector,
+                private zone: NgZone) {
         super();
     }
 
     handleError(errorResponse: HttpErrorResponse | any) {
        if(errorResponse instanceof HttpErrorResponse) {
            const message = errorResponse.error.message;
-           switch (errorResponse.status) {
+           //resolve problem about to run some code outside some zone.
+           this.zone.run(() => {
+
+            switch (errorResponse.status) {
                case 401:
                    this.injector.get(LoginService).handleLogin();
                    break;
@@ -30,7 +35,9 @@ export class ApplicationErrorHandler extends ErrorHandler {
                
                default:
                    break;
-           }
+                   
+              }
+          });    
        } 
        super.handleError(errorResponse);
     }
